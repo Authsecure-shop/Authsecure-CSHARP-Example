57a7d72461c785ec0b8a7a6e759d0e8f57eb2e87
Authsecure <teamdeveloperxd@gmail.com>
2025-11-11 08:20:10 -0800

    Date/time error display

--- Form/Main.cs
+++ Form/Main.cs
@@ -32,13 +32,13 @@ namespace AuthSecure
         private async void Main_Load(object sender, EventArgs e)
         {
             userDataField.Items.Add($"Username: {Login.AuthSecureApp.user_data.username}");
-            userDataField.Items.Add($"License: {Login.AuthSecureApp.user_data.subscriptions[0].key}"); // this can be used if the user used a license, username, and password for register. It'll display the license assigned to the user                                                                                                      
-            userDataField.Items.Add($"Expires: {Login.AuthSecureApp.user_data.subscriptions[0].expiration}"); // Directly DateTime dikhane ke liye
+            userDataField.Items.Add($"License: {Login.AuthSecureApp.user_data.subscriptions[0].key}");  // this can be used if the user used a license, username, and password for register. It'll display the license assigned to the user 
+            userDataField.Items.Add($"Expires: {Login.AuthSecureApp.user_data.subscriptions[0].expiration}");
             userDataField.Items.Add($"Subscription: {Login.AuthSecureApp.user_data.subscriptions[0].subscription}");
             userDataField.Items.Add($"IP: {Login.AuthSecureApp.user_data.ip}");
             userDataField.Items.Add($"HWID: {Login.AuthSecureApp.user_data.hwid}");
-            userDataField.Items.Add($"Creation Date: {Login.AuthSecureApp.user_data.createdate}"); // this has a capital "C" , if you use a lowercase "c" it won't convert unix
-            userDataField.Items.Add($"Last Login: {Login.AuthSecureApp.user_data.lastlogin}"); // this has a capital "L", if you use a lowercase "l" it won't convert unix
+            userDataField.Items.Add($"Creation Date: {UnixToDateTime(long.Parse(Login.AuthSecureApp.user_data.createdate))}"); // this has a capital "C" , if you use a lowercase "c" it won't convert unix
+            userDataField.Items.Add($"Last Login: {UnixToDateTime(long.Parse(Login.AuthSecureApp.user_data.lastlogin))}"); // this has a capital "L", if you use a lowercase "l" it won't convert unix
             userDataField.Items.Add($"Time Left: {Login.AuthSecureApp.expirydaysleft()}");
         }
 
@@ -85,3 +85,4 @@ namespace AuthSecure
         }
     }
 }
+
